cf144fd6d8cb9bab7b2291c4e548b7608430bd11
Luke Schnetlage <lschnetlage@my.apsu.edu>
2023-08-10 19:26:12 -0500

    created react app

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <iframe 
+        width="560" 
+        height="315" 
+        src="https://www.youtube-nocookie.com/embed/aV63-4f88o8" 
+        title="YouTube video player" 
+        frameBorder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+        allowFullScreen>
+      </iframe>
       </header>
     </div>
   );
